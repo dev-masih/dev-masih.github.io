a55a972ab95e05837d1875a5c8cff761b9850b18
Masih Akbari <akbari.masih@gmail.com>
2025-12-19 14:16:07 +0330

    update info

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -20,7 +20,7 @@ const CONFIG = {
         limit: 8, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: ['dev-masih/my-media-bin', 'dev-masih/dev-masih.github.io'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
