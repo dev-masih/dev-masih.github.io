eab79d0576ed0ced983405691d39ba789b54c946
Masih Akbari <akbari.masih@gmail.com>
2025-12-20 12:02:35 +0330

    update info

--- src/components/blog-card/index.tsx
+++ src/components/blog-card/index.tsx
@@ -159,7 +159,7 @@ const BlogCard = ({
                           className="py-2 px-4 text-xs leading-3 rounded-full bg-base-300 mr-1 mb-1 opacity-50 text-base-content"
                           key={index2}
                         >
-                          #{category}
+                          {category}
                         </div>
                       ))}
                     </div>
